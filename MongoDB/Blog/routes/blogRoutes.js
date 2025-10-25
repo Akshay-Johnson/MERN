60d3af7f373c15867a1a41d619f6a9b8e451611e
R0NIN007 <akshayjohnson17@gmail.com>
2025-10-25 22:40:11 +0530

    blog remaining

--- MongoDB/Blog/routes/blogRoutes.js
+++ MongoDB/Blog/routes/blogRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import Blog from '../models/blogModel.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
@@ -37,6 +38,54 @@ router.get('/',async(req,res)=>{
     res.json(blogs);
 });
 
+
+//filter
+router.get("/filter",async(req,res)=>{
+    try{
+        const {author,keyword,sortBy,order} = req.query;
+
+        let filter = {};
+        if(author) {
+            if(mongoose.Types.ObjectId.isValid(author)){
+                filter.author = new mongoose.Types.ObjectId(author);
+            } else {
+                return res.status(400).json({message:"Invalid author ID"});
+            }
+        }
+
+        if(keyword) {
+            filter.$or = [
+                {title:{$regex:keyword,$options:"i"}},
+                {content:{$regex:keyword,$options:"i"}}
+            ];
+        }
+
+        let sort ={};
+        if(sortBy){
+            if(sortBy ==="likes"){
+                sort={"likes.length": order ==="desc"? -1:1};
+            } else {
+                sort[sortBy] = order ==="desc"? -1:1;
+            }
+        }else{
+            sort={createdAt:-1};
+        }
+    
+
+       console.log("Filter Used:",filter);
+       console.log("Sort Used:",sort);
+
+       const blogs= await Blog.find(filter)
+         .populate("author","displayname email")
+         .sort(sort);
+        console.log("Filter used:", JSON.stringify(filter, null, 2));
+        console.log("Sort used:", JSON.stringify(sort, null, 2));
+       res.json(blogs);
+    }catch(error){
+        res.status(500).json({message:"Error filtering blog posts"});
+    }
+});
+
 //get single blog post
 router.get("/:id",async(req,res)=>{
     const blog = await Blog.findById(req.params.id).populate("author","displayname email");
@@ -57,5 +106,96 @@ router.delete("/:id",auth,async(req,res)=>{
     await Blog.findByIdAndDelete(req.params.id);
     res.json({message:"Blog post deleted"});
 });
+
+//comment on blog post
+router.post("/:id/comments",auth,async(req,res)=>{
+    try{
+        const{text} = req.body;
+        if(!text){
+            return res.status(400).json({message:"Comment text is required"});
+        }
+
+        const blog = await Blog.findById(req.params.id);
+        if(!blog){
+            return res.status(404).json({message:"Blog post not found"});
+        }
+
+        blog.comments.push({ user:req.user,text});
+        await blog.save();
+
+    }catch(error){
+        res.status(500).json({message:"Error adding comment"});
+    }
+});
+
+//update comment
+router.put("/:blogId/comments/:commentId",auth,async(req,res)=>{
+    try{
+        const blog = await Blog.findById(req.params.blogId);
+        if(!blog){
+            return res.status(404).json({message:"Blog post not found"});
+        }
+        const comment = blog.comments.id(req.params.commentId);
+        if(!comment){
+            return res.status(404).json({message:"Comment not found"});
+        }
+        if(comment.user.toString() !== req.user){
+            return res.status(403).json({message:"Unauthorized"});
+        }
+        comment.text= req.body.text || comment.text;
+        await blog.save();
+        res.json({message:"Comment updated",comment});
+    }catch(error){
+        res.status(500).json({message:"Error updating comment"});   
+    }
+});
+
+//delete comment
+router.delete("/:blogId/comments/:commentId",auth,async(req,res)=>{
+    try{
+        const blog = await Blog.findById(req.params.blogId);
+        if(!blog){
+            return res.status(404).json({message:"Blog post not found"});
+        }
+
+        const comment = blog.comments.id(req.params.commentId);
+        if(!comment){
+            return res.status(404).json({message:"Comment not found"});
+        }
+        if(comment.user.toString() !== req.user){
+            return res.status(403).json({message:"Unauthorized"});
+        }
+        comment.deleteOne();
+        await blog.save();
+        res.json({message:"Comment deleted"});
+
+    }catch(error){
+        res.status(500).json({message:"Error deleting comment"});   
+    }
+});
+
+//blog like and unlike
+router.post("/:id/like",auth,async(req,res)=>{
+    try{
+        const blog = await Blog.findById(req.params.id);
+        if(!blog){
+            return res.status(404).json({message:"Blog post not found"});
+        }
+
+        const alreadyLiked = blog.likes.includes(req.user);
+        if(alreadyLiked){
+            blog.likes.pull(req.user);
+        }else{
+            blog.likes.push(req.user);
+        }
+        await blog.save();
+        res.json({message:alreadyLiked?" Unliked":"Liked",likes:blog.likes});
+    }catch(error){
+        res.status(500).json({message:"Error liking/unliking blog post"});
+    }
+});
+
+
+
 
 export default router;
